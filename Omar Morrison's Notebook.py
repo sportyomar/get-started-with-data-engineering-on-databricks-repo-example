1a358063c731e8fd586b6ee1b835e4afc3bd2005
sportyomar <sportyomar>
2024-03-29 02:58:58 +0000

    math update

---
+++ Omar Morrison's Notebook.py
@@ -0,0 +1,17 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ### This is a math workbook.
+
+# COMMAND ----------
+
+def math():
+    solution = 1 + 1
+    return solution
+
+# COMMAND ----------
+
+math()
+
+# COMMAND ----------
+
+
